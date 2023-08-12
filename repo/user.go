7d4514d468aee14f459ece0d360c2d770d73222d
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-08-12 18:56:58 +0300

    📦 NEW: Implement repos

---
+++ repo/user.go
@@ -0,0 +1,29 @@
+package repo
+
+import (
+	"github.com/tiptophelmet/nomess/db"
+	"github.com/tiptophelmet/nomess/model"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type User struct {
+	db *db.MongoDB
+}
+
+func InitUserRepo() *User {
+	return &User{db: db.InitMongoDB()}
+}
+
+func (repo *User) Save(model *model.User) (*mongo.InsertOneResult, error) {
+	defer repo.db.CtxCancel()
+	defer repo.db.Client.Disconnect(repo.db.Ctx)
+	collection := repo.db.Client.Database("mydb").Collection("user")
+
+	result, err := collection.InsertOne(repo.db.Ctx, model)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
---
+++ repo/userVerification.go
@@ -0,0 +1,29 @@
+package repo
+
+import (
+	"github.com/tiptophelmet/nomess/db"
+	"github.com/tiptophelmet/nomess/model"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserVerification struct {
+	db *db.MongoDB
+}
+
+func InitUserVerificationRepo() *UserVerification {
+	return &UserVerification{db: db.InitMongoDB()}
+}
+
+func (repo *UserVerification) Save(model *model.UserVerification) (*mongo.InsertOneResult, error) {
+	defer repo.db.CtxCancel()
+	defer repo.db.Client.Disconnect(repo.db.Ctx)
+	collection := repo.db.Client.Database("mydb").Collection("userVerification")
+
+	result, err := collection.InsertOne(repo.db.Ctx, model)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
