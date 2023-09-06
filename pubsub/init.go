16e284c168ad716f18e2cc0035838c1d9028d8a1
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-06 12:06:02 +0200

    👌 IMPROVE: Make pubsub thread-safe

--- pubsub/init.go
+++ pubsub/init.go
@@ -1,29 +1,39 @@
 package pubsub
 
 import (
+	"sync"
+
 	"github.com/tiptophelmet/nomess/config"
 	"github.com/tiptophelmet/nomess/logger"
 	"github.com/tiptophelmet/nomess/pubsub/broker"
 )
 
-var pubsubClient broker.PubSubBroker
+type PubSubClient struct {
+	broker broker.PubSubBroker
+	mu     sync.Mutex
+}
 
-func InitPubSub() {
+var pubsubClient *PubSubClient
+
+func Init() {
 	driverConfig := config.Get("pubsub.driver").Required().Str()
 
 	switch driverConfig {
 	case "redis":
-		pubsubClient = &broker.RedisBroker{}
+		pubsubClient = &PubSubClient{broker: &broker.RedisBroker{}}
 	case "nats":
-		pubsubClient = &broker.NATSBroker{}
+		pubsubClient = &PubSubClient{broker: &broker.NATSBroker{}}
 	default:
-		logger.Panic("unsupported cache.driver: %v", driverConfig)
+		logger.Panic("unsupported pubsub.driver: %v", driverConfig)
 	}
 
 	driverURL := config.Get("pubsub.url").Required().Str()
-	pubsubClient.Connect(driverURL)
+	pubsubClient.broker.Connect(driverURL)
 }
 
 func Connection() broker.PubSubBroker {
-	return pubsubClient
+	pubsubClient.mu.Lock()
+	defer pubsubClient.mu.Unlock()
+
+	return pubsubClient.broker
 }
