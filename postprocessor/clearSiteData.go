97d3456db3291618f1873c5e04493f721fd9007c
tiptophelmet <serhii.ponomarenko.jobs@gmail.com>
2023-09-13 20:19:20 +0200

    📦 NEW: Clear-site-data postprocessor

---
+++ postprocessor/clearSiteData.go
@@ -0,0 +1,20 @@
+package postprocessor
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func clearSiteData(directives ...string) string {
+	var directiveStr string
+	for _, d := range directives {
+		directiveStr += fmt.Sprintf("\"%s\", ", d)
+	}
+
+	return strings.TrimRight(directiveStr, ", ")
+}
+
+func WithClearSiteData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Clear-Site-Data", clearSiteData("cache", "cookies", "storage", "executionContexts"))
+}
